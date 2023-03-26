10f79ad6e28feea92232eaef6f20fe678f1a1a8d
“1920466903@qq.com” <1920466903@qq.com>
2023-03-26 10:26:33 +0800

    commit 输入三个数并从小到大输出 file

---
+++ 输入三个数并从小到大输出/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() 
+{
+	int a,b,c;
+	scanf("%d%d%d",&a,&b,&c);
+	if(a<b)
+	{
+		int n=a;
+		a=b;
+		b=n;
+	}
+		if(a<c)
+	{
+		int n=a;
+		a=c;
+		c=n;
+	}
+		if(b<c)
+	{
+		int n=b;
+		b=c;
+		c=n;
+	}
+	printf("%d %d %d",a,b,c);
+	return 0;
+}
