3b1cdb792fadad2129d15cb5a5c71fc59af62710
Jeffreykakk <1920466903@qq.com>
2023-04-05 13:08:28 +0800

    4-5

---
+++ 十进制转八进制/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int convert(int Decimalnumber)
+{
+	int Octalnumber = 0, i = 1;
+    while (Decimalnumber != 0)
+	{
+		Octalnumber += (Decimalnumber % 8) * i;
+        Decimalnumber /= 8;
+        i *= 10;
+    }
+	return Octalnumber;
+}
+
+int main()
+{
+	int Decimalnumber;
+    printf("Decima lnumber: ");
+    scanf("%d", &Decimalnumber);
+ 
+    printf("Decima lnumber %d --> Octal number %d", Decimalnumber, convert(Decimalnumber));
+ 
+    return 0;
+}
