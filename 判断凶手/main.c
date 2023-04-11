9966f1138dee4b3d4d987f731788e6612432e35e
Jeffreykakk <1920466903@qq.com>
2023-04-11 13:34:06 +0800

    4-11

---
+++ 判断凶手/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+	char killer=0;
+	for(killer='A';killer<='D';killer++)
+	{
+		if( ( (killer!='A') + (killer=='C') +(killer=='D') + (killer!='D') ) ==3 )
+		printf("%c",killer);
+	}
+	return 0;
+}
