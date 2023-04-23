8b667b01d1139b0db077792daf76acfb31a9c1a8
Jeffreykakk <1920466903@qq.com>
2023-04-23 13:26:09 +0800

    4-23

---
+++ 使用malloc动态内存开辟/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+	int* p=(int*)malloc(10*sizeof(int));
+	if(p==NULL)
+	{
+		perror("main");
+		return 1;
+	}
+	int i=0;
+	for(i=0;i<10;i++)
+	printf("%d ",p[i]);
+	printf("\n");
+	for(i=0;i<10;i++)
+	p[i]=i;
+	for(i=0;i<10;i++)
+	printf("%d ",p[i]);
+	free(p);
+	p=NULL;
+	return 0;
+}
