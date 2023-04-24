82b71408da923f4396dc76a3473baf90a75cc16d
Jeffreykakk <1920466903@qq.com>
2023-04-24 21:24:55 +0800

    4-24

---
+++ 约瑟夫环问题（输出整个过程）2.0/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=0,j=0,i=0,m=0,k=0,lim=0;
+    scanf("%d %d",&n,&lim);
+    int num[n];
+    for (j=0;j<n;j++)
+    num[j]=j+1;
+    while(m<n-1)
+    {
+        if (num[i]!=0)
+        k++;
+        if (k==lim)
+        {
+            k=0;
+            printf("%d ",num[i]);
+            num[i]=0;
+            m++;
+        }
+        i++;
+        if (i==n)
+        i=0;
+    }
+	for (j=0;j<n;j++)
+    {
+        if (num[j]!=0)
+        printf("%d",num[j]);
+    }
+}
