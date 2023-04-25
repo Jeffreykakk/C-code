b7760fc567e203abae133e80d2b182787152c6f0
Jeffreykakk <1920466903@qq.com>
2023-04-25 12:31:56 +0800

    4-25

---
+++ 约瑟夫环问题（输出全过程）3.0/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int josef(int a[],int n,int m)
+{
+    int i=0,j=0,k=0;
+    for(i=0;i<n;i++)
+    {
+        j=1;
+        while(j<m)
+        {
+            while(a[k]==0)
+            k=(k+1)%n;
+            j++;
+            k=(k+1)%n;
+        }
+        while(a[k]==0)
+        k=(k+1)%n;
+        printf("%d ",a[k]);
+        a[k]=0;
+    }
+    return 0;
+}
+int main()
+{
+    int i=0,n=0,m=0;
+    scanf("%d%d",&n,&m);
+    int a[n];
+    for(i=0;i<n;i++)
+    a[i]=i+1;
+    josef(a,n,m);
+    return 0;
+}
