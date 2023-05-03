eb823f92a70d84bab1c017558d57e4e579147f09
Jeffreykakk <1920466903@qq.com>
2023-05-03 10:36:13 +0800

    5-3

---
+++ 用fputc从标准输出流中写入数据/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+	fputc('J',stdout);
+	fputc('e',stdout);
+	fputc('f',stdout);
+	fputc('f',stdout);
+	fputc('r',stdout);
+	fputc('e',stdout);
+	fputc('y',stdout);
+	return 0;
+}
