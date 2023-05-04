718c68af6683512c0f4ffaaae4bc02384805eba4
Jeffreykakk <1920466903@qq.com>
2023-05-04 10:42:17 +0800

    5-4

---
+++ 递归函数计算1到n之和/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int sum( int n );
+
+int main()
+{
+    int n=0;
+    scanf("%d", &n);
+    printf ("%d\n", sum(n));
+    return 0;
+}
+
+int sum( int n )
+{
+	if(n==1)
+	return 1;
+	else
+	return n+sum(n-1);
+}
