f348e37537823c06d7dcc2eb8e15e184fa7f73d9
Jeffreykakk <1920466903@qq.com>
2023-05-04 10:54:52 +0800

    5-4

---
+++ 可保存的通讯录/contact.h
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define max_name 20
+#define max_sex 10
+#define max_tele 12
+#define max_addr 30
+#define default_sz 3
+#define default_increase 2
+
+typedef struct PeoInfo
+{
+	char name[max_name];
+	char sex[max_sex];
+	int age;
+	char tele[max_tele];
+	char addr[max_addr];
+}PeoInfo;
+
+typedef struct Contact
+{
+	PeoInfo* data;
+	int sz;
+	int capacity;
+}Contact;
+
+void InitContact(Contact *pc);
+void Contact_add(Contact *pc);
+void Contact_print(const Contact *pc);
+void Contact_delete(Contact *pc);
+void Contact_search(const Contact *pc);
+void Contact_modify(Contact *pc);
+void Contact_sort(Contact *pc);
+void Contact_destroy(Contact *pc);
+void check_capacity(Contact *pc);
+void Contact_save(Contact *pc);
+void Contact_load(Contact *pc);
