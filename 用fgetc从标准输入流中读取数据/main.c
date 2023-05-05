9353819e991e3f5d5c1d096486a1353ab9adaea1
Jeffreykakk <1920466903@qq.com>
2023-05-05 12:37:54 +0800

    5-5

---
+++ 用fgetc从标准输入流中读取数据/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+	int ret=fgetc(stdin);
+	printf("%c",ret);
+	ret=fgetc(stdin);
+	printf("%c",ret);
+	ret=fgetc(stdin);
+	printf("%c",ret);
+	ret=fgetc(stdin);
+	printf("%c",ret);
+	ret=fgetc(stdin);
+	printf("%c",ret);
+	ret=fgetc(stdin);
+	printf("%c",ret);
+	ret=fgetc(stdin);
+	printf("%c",ret);
+	return 0;
+}
