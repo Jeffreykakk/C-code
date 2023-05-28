f4dc3e82b4087634e8a1cfb6697edc1bdfc19e3f
Jeffreykakk <1920466903@qq.com>
2023-05-28 10:07:52 +0800

    5-28

---
+++ BinaryTreeDestroy/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int BTDataType;
+
+typedef struct BinaryTreeNode
+{
+    struct BinaryTreeNode* left;
+    struct BinaryTreeNode* right;
+    BTDataType data;
+}BTNode;
+
+BTNode* BuyNode(BTDataType x)
+{
+    BTNode* newnode=(BTNode*)malloc(sizeof(BTNode));
+    if(newnode==NULL)
+    exit(-1);
+    newnode->left=newnode->right=NULL;
+    newnode->data=x;
+    return newnode;
+}
+
+BTNode* CreateBinaryTree()
+{
+    BTNode* node1 = BuyNode(1);
+    BTNode* node2 = BuyNode(2);
+    BTNode* node3 = BuyNode(3);
+    BTNode* node4 = BuyNode(4);
+    BTNode* node5 = BuyNode(5);
+    BTNode* node6 = BuyNode(6);
+    
+    node1->left = node2;
+    node1->right = node4;
+    node2->left = node3;
+    node4->left = node5;
+    node4->right = node6;
+    return node1;
+}
+
+
+
+void BinaryTreeDestroy(BTNode* root)
+{
+    if(root==NULL)
+    return;
+    BinaryTreeDestroy(root->left);
+    BinaryTreeDestroy(root->right);
+    free(root);
+}
+
+int main()
+{
+    BTNode* root=CreateBinaryTree();
+    BinaryTreeDestroy(root);
+    return 0;
+}
