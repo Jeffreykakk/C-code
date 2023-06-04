3bccb1eb9760ca732480661eda1eb622e83cd29c
Jeffreykakk <1920466903@qq.com>
2023-06-04 13:40:58 +0800

    6-4

---
+++ MergeSortNonR2.0/main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+
+//归并排序，将两个有序数据合并，使数据仍然有序
+//将一个待排数据分割成两个待排区间，递归使两个区间有序
+
+void printarr(int* arr,int size)
+{
+    int i=0;
+    for(i=0;i<size;i++)
+    printf("%d ",arr[i]);
+    printf("\n");
+}
+
+
+
+
+void MergeSort(int* arr, int begin, int end)
+{
+    int size=end-begin+1;
+    int* tmp=(int*)malloc(sizeof(int)*size);
+    int gap=1;
+    while(gap<size)
+    {
+        int i=0,j=0;
+        for(i=0;i<size;i+=2*gap)
+        {
+            //归并
+            int begin1=i,end1=begin1+gap-1;
+            int begin2=begin1+gap,end2=begin1+2*gap-1;
+            //修正边界
+            if(end1>=size)
+            break;
+            else if(begin2>=size)
+            break;
+            else if(end2>=size)
+            end2=size-1;
+            while(begin1<=end1&&begin2<=end2)
+            {
+                if(arr[begin1]<=arr[begin2])
+                tmp[j++]=arr[begin1++];
+                else
+                tmp[j++]=arr[begin2++];
+            }
+            while(begin1<=end1)
+            tmp[j++]=arr[begin1++];
+            while(begin2<=end2)
+            tmp[j++]=arr[begin2++];
+            memcpy(arr+i,tmp+i,sizeof(int)*(end2-i+1));
+        }
+        gap*=2;
+    }
+    free(tmp);
+}
+
+
+
+void MergeSort_test()
+{
+    int size=30;
+    srand((unsigned int)time(NULL));
+    int* arr=(int*)malloc(sizeof(int)*size);
+    int i=0;
+    for(i=0;i<size;i++)
+    arr[i]=rand()%100;
+    printarr(arr,size);
+    MergeSort(arr,0,size-1);
+    printarr(arr,size);
+    
+    free(arr);
+
+}
+
+int main()
+{
+    MergeSort_test();
+    system("pause");
+    return 0;
+}
