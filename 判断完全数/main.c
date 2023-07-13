a87519155a14d57e99b261556953fe561dcc4baf
Jeffreykakk <1920466903@qq.com>
2023-07-13 13:47:18 +0800

    7-13

--- 判断完全数/main.c
+++ 判断完全数/main.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+bool checkPerfectNumber(int num)
+{
+    if (num == 1)
+        return false;
+    int i = 0, sum = 1;
+    double end = sqrt(num);
+    for (i = 2; i <= end; i++)
+    {
+        if (num % i == 0)
+        {
+            if (i < end)
+                sum = sum + i + num / i;
+            else
+                sum += i;
+        }
+    }
+    if (sum == num)
+        return true;
+    else
+        return false;
+}
 
 int main()
 {
-    int i=0,a=0,sum=0;
-    scanf("%d",&a);
-	for(i=1;i<a;i++)
-    {
-        if(a%i==0)
-        {
-            sum+=i;
-    	}
-    }
-    if(sum==a)
-    printf("yes\n");
+    int ret = checkPerfectNumber(6);
+    printf("%d\n", ret);
     return 0;
 }
